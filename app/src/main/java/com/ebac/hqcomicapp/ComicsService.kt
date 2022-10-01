2e38041cb856dbad9bbf13374f8e547b928dc005
Gisele <gisesonia@gmail.com>
2022-10-01 13:57:18 -0300

    Modificando a service e a ModelView para usar coroutine

--- app/src/main/java/com/ebac/hqcomicapp/ComicsService.kt
+++ app/src/main/java/com/ebac/hqcomicapp/ComicsService.kt
@@ -1,16 +1,16 @@
 package com.ebac.hqcomicapp
 
 import com.ebac.hqcomicapp.data.ComicResponse
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ComicsService {
     @GET("v1/public/comics")
-    fun getComicsList(
+    suspend fun getComicsList(
         @Query("ts") timestamp: String,
         @Query("apikey") publicKey: String,
         @Query("hash") hash: String,
         @Query("limit") limit: Int
-    ) : Call<ComicResponse>
+    ): Response<ComicResponse>
 }
--- app/src/main/java/com/ebac/hqcomicapp/HQViewModel.kt
+++ app/src/main/java/com/ebac/hqcomicapp/HQViewModel.kt
@@ -3,22 +3,19 @@ package com.ebac.hqcomicapp
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.ebac.hqcomicapp.HQDetails.HQDetails
 import com.ebac.hqcomicapp.data.Comic
-import com.ebac.hqcomicapp.data.ComicResponse
 import com.ebac.hqcomicapp.data.DataState
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 class HQViewModel : ViewModel() {
-    //primeiro pega como tipo LiveData que não é modificável, só de leitura
-    val hqDetailsLiveData: LiveData<HQDetails>
+
+    val hqDetailsLiveData: LiveData<HQDetails>//primeiro pega como tipo LiveData que não é modificável, só de leitura
         get() = _hqDetailsLiveData
-    //transforma em dados mutáveis
-    private val _hqDetailsLiveData = MutableLiveData<HQDetails>()
+    private val _hqDetailsLiveData = MutableLiveData<HQDetails>() //transforma em dados mutáveis
 
     val hqListLiveData: LiveData<List<Comic>?>
         get() = _hqListLiveData
@@ -44,36 +41,26 @@ class HQViewModel : ViewModel() {
         getHqData()
     }
 
-    fun onHQSelected(position: Int){
+    fun onHQSelected(position: Int) {
         val hqDetails = HQDetails("Minha HQ", "Este é um conteúdo de texto extenso")
         _hqDetailsLiveData.postValue(hqDetails)
         _navigationToDetailLiveData.postValue(Unit)
     }
 
-    private fun getHqData(){
+    private fun getHqData() {
         val timestamp = ApiHelper.getCurrentTimeStamp()
         val input = "$timestamp${ApiCredentials().privateKey}${ApiCredentials().publicKey}"
-
         val hash = ApiHelper.generateMD5Hash(input)
 
-        comicsService.getComicsList(timestamp, ApiCredentials().publicKey, hash, 100).enqueue(object:
-            Callback<ComicResponse> {
-            override fun onResponse(call: Call<ComicResponse>, response: Response<ComicResponse>) {
-                if(response.isSuccessful){
-                    _hqListLiveData.postValue(response.body()?.data?.results)
-                    _appState.postValue(DataState.Success)
-                } else {
-                    _appState.postValue(DataState.Error)
-                }
-            }
-
-            override fun onFailure(call: Call<ComicResponse>, t: Throwable) {
-               /* t?.message?.let {
-                    Log.e("onFailure error", t?.message!!)
-                }*/
+        viewModelScope.launch {
+            val response =
+                comicsService.getComicsList(timestamp, ApiCredentials().publicKey, hash, 100)
+            if (response.isSuccessful) {
+                _hqListLiveData.postValue(response.body()?.data?.results)
+                _appState.postValue(DataState.Success)
+            } else {
                 _appState.postValue(DataState.Error)
             }
-
-        })
+        }
     }
 }
